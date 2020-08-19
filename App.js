de12e62b7185fa7e5c8709fef874a5943885846d
Justin Robinson <jrobi9091@gmail.com>
2020-08-19 19:09:54 +1000

    Added better login, and looks a bit better

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useEffect, useState } from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
-
-  const [api, setApi] = useState("Empty");
+  const [api, setApi] = useState("Logged out (No User)");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
   const clickFunctionTestLoggedIn = () => {
     fetch('https://pylicht.pythonanywhere.com/testloggedin', {'method': 'GET'})
@@ -13,7 +14,16 @@ export default function App() {
   }
 
   const clickFunctionLogin = () => {
-    fetch('https://pylicht.pythonanywhere.com/badlogin', {'method': 'POST'})
+    let formdata = new FormData();
+    formdata.append("username", username);
+    formdata.append("password", password);
+    setUsername('');
+    setPassword('');
+    
+    fetch('https://pylicht.pythonanywhere.com/login',{
+      'method': 'POST',
+      'headers': {'Content-Type': 'multipart/form-data'},
+      'body': formdata})
     .then((response) => { response.text().then(function (text) {setApi(text)}); })
     .catch((error) => setApi(error.message));
   }
@@ -27,19 +37,41 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text style={styles.header}>This is Diego Dollars :)</Text>
-      <Text>({api})</Text>
+      <Text>Status: {api}</Text>
+
+      <View style={styles.loginbox}>
+        <TextInput 
+          style={styles.hairline}
+          placeholder="Username"
+          onChangeText={(username) => (setUsername(username))}
+          value={username}
+        />
+        <TextInput 
+          style={styles.hairline}
+          secureTextEntry={true}
+          placeholder="Password"
+          onChangeText={(password) => (setPassword(password))}
+          value={password}
+        />
+      </View>
+
+      <View style={styles.loginbuttonbox}>
       <Button 
         title="TestLoggedIn" 
+        style={styles.loginbutton}
         onPress={clickFunctionTestLoggedIn}
       />
       <Button 
         title="Login" 
+        style={styles.loginbutton}
         onPress={clickFunctionLogin}
       />
       <Button 
-        title="Logout" 
+        title="Logout"
+        style={styles.loginbutton} 
         onPress={clickFunctionLogout}
       />
+      </View>
       <StatusBar style='auto' />
     </View>
   );
@@ -55,4 +87,23 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  loginbuttonbox: {
+    padding: 24,
+    width: '100%',
+  },
+  loginbutton: {
+    paddingVertical: 10,
+  },
+  hairline: {
+    width: '100%',
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    borderBottomColor: "blue",
+    borderBottomWidth: StyleSheet.hairlineWidth
+  },
+  loginbox: {
+    width: '100%',
+    paddingHorizontal: 35,
+    paddingVertical: 10
+  }
 });
