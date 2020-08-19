02a1d4f4e4f048af5018f0b5a51968eb6dc09e6f
Justin Robinson <jrobi9091@gmail.com>
2020-08-19 15:17:46 +1000

    Login tests should work

--- App.js
+++ App.js
@@ -1,11 +1,45 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
+
+  const [api, setApi] = useState("Empty");
+
+  const clickFunctionTestLoggedIn = () => {
+    fetch('https://pylicht.pythonanywhere.com/testloggedin', {'method': 'GET'})
+    .then((response) => { response.text().then(function (text) {setApi(text)}); })
+    .catch((error) => setApi(error.message));
+  }
+
+  const clickFunctionLogin = () => {
+    fetch('https://pylicht.pythonanywhere.com/badlogin', {'method': 'POST'})
+    .then((response) => { response.text().then(function (text) {setApi(text)}); })
+    .catch((error) => setApi(error.message));
+  }
+
+  const clickFunctionLogout = () => {
+    fetch('https://pylicht.pythonanywhere.com/logout', {'method': 'GET'})
+    .then((response) => { response.text().then(function (text) {setApi(text)}); })
+    .catch((error) => setApi(error.message));
+  }
+
   return (
     <View style={styles.container}>
-      <Text style={styles.header}>This is Diego Dollars</Text>
+      <Text style={styles.header}>This is Diego Dollars :)</Text>
+      <Text>({api})</Text>
+      <Button 
+        title="TestLoggedIn" 
+        onPress={clickFunctionTestLoggedIn}
+      />
+      <Button 
+        title="Login" 
+        onPress={clickFunctionLogin}
+      />
+      <Button 
+        title="Logout" 
+        onPress={clickFunctionLogout}
+      />
       <StatusBar style='auto' />
     </View>
   );
