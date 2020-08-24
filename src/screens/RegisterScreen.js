a40a39ebeb16ffe6ab04c278b1a396fb2b4639ed
Bryley Hayter <bryley@pop-os.localdomain>
2020-08-24 19:14:21 +1000

    Reorganised some files and added text box style.

--- src/screens/RegisterScreen.js
+++ src/screens/RegisterScreen.js
@@ -1,53 +1,53 @@
 import React, { useState } from "react";
-import { STYLE_SHEET } from "../css/stylesheet";
+import { STYLE_SHEET } from "../styles/stylesheet";
 import { TextInput } from "react-native-gesture-handler";
 import { Text, Button, View } from "react-native";
 
 export default function RegisterScreen({ navigation }) {
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-    const [passwordCheck, setPasswordCheck] = useState("");
-  
-    return(
-      <View style={STYLE_SHEET.container}>
-        <Text style={STYLE_SHEET.header}>Register Page</Text>
-        <Text>(Doesn't work yet. Work on this later)</Text>
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordCheck, setPasswordCheck] = useState("");
 
-        <View style={STYLE_SHEET.loginbox}>
-          <TextInput
-            style={STYLE_SHEET.hairline}
-            placeholder="Username"
-            onChangeText={(username) => (setUsername(username))}
-            value={username}
-          />
-          <TextInput 
-            style={STYLE_SHEET.hairline}
-            secureTextEntry={true}
-            placeholder="Password"
-            onChangeText={(password) => (setPassword(password))}
-            value={password}
-          />
-          <TextInput 
-            style={STYLE_SHEET.hairline}
-            secureTextEntry={true}
-            placeholder="Confirm Password"
-            onChangeText={(passwordCheck) => (setPasswordCheck(passwordCheck))}
-            value={passwordCheck}
-          />
-        </View>
-  
-        <Text 
-          style={STYLE_SHEET.helpmessage}
-        >Passwords do not match.</Text>
-  
-        <View style={STYLE_SHEET.loginbuttonbox}>
-        <Button 
-          title="Register" 
+  return (
+    <View style={STYLE_SHEET.container}>
+      <Text style={STYLE_SHEET.header}>Register Page</Text>
+      <Text>(Doesn't work yet. Work on this later)</Text>
+
+      <View style={STYLE_SHEET.loginbox}>
+        <TextInput
+          style={STYLE_SHEET.textInput}
+          placeholder="Username"
+          onChangeText={(username) => setUsername(username)}
+          value={username}
+        />
+        <TextInput
+          style={STYLE_SHEET.textInput}
+          secureTextEntry={true}
+          placeholder="Password"
+          onChangeText={(password) => setPassword(password)}
+          value={password}
+        />
+        <TextInput
+          style={STYLE_SHEET.textInput}
+          secureTextEntry={true}
+          placeholder="Confirm Password"
+          onChangeText={(passwordCheck) => setPasswordCheck(passwordCheck)}
+          value={passwordCheck}
+        />
+      </View>
+
+      <Text style={STYLE_SHEET.helpmessage}>Passwords do not match.</Text>
+
+      <View style={STYLE_SHEET.loginbuttonbox}>
+        <Button
+          title="Register"
           style={STYLE_SHEET.loginbutton}
           //onPress={()}
-          disabled={!(password == passwordCheck) || (username == "") || (password == "")}
+          disabled={
+            !(password == passwordCheck) || username == "" || password == ""
+          }
         />
-        </View>
       </View>
-    );
-  }
+    </View>
+  );
+}
--- src/screens/UserScreen.js
+++ src/screens/UserScreen.js
@@ -1,47 +1,53 @@
 import React, { useState } from "react";
 import { API_LOGOUT, API_TEST_LOGGED_IN } from "../helper/constants";
 import navigateAndReset from "../helper/functions";
-import { STYLE_SHEET } from "../css/stylesheet";
+import { STYLE_SHEET } from "../styles/stylesheet";
 import { Text, Button, View } from "react-native";
 
 export default function UserScreen({ navigation }) {
-    const [api, setApi] = useState("None");
-  
-    const clickFunctionLogout = () => {
-      fetch(API_LOGOUT, {'method': 'GET'})
-      .then((response) => { return response.text().then(function (text) { return text;});})
-      .then((res) =>
-        {
-          if (res.includes("Successfully logged out!")) {
-            navigateAndReset(navigation, "Login");
-          }
+  const [api, setApi] = useState("None");
+
+  const clickFunctionLogout = () => {
+    fetch(API_LOGOUT, { method: "GET" })
+      .then((response) => {
+        return response.text().then(function (text) {
+          return text;
+        });
+      })
+      .then((res) => {
+        if (res.includes("Successfully logged out!")) {
+          navigateAndReset(navigation, "Login");
         }
-      )
+      })
       .catch((error) => setApi(error.message));
-    }
-  
-    const clickFunctionTestLoggedIn = () => {
-      fetch(API_TEST_LOGGED_IN, {'method': 'GET'})
-      .then((response) => { response.text().then(function (text) {setApi(text)}); })
+  };
+
+  const clickFunctionTestLoggedIn = () => {
+    fetch(API_TEST_LOGGED_IN, { method: "GET" })
+      .then((response) => {
+        response.text().then(function (text) {
+          setApi(text);
+        });
+      })
       .catch((error) => setApi(error.message));
-    }
-  
-    return(
-      <View style={STYLE_SHEET.container}>
-        <Text style={STYLE_SHEET.header}>Hello User</Text>
-        <Text>Status: {api}</Text>
-        <View style={STYLE_SHEET.loginbuttonbox}>
-            <Button 
-              title="TestLoggedIn" 
-              style={STYLE_SHEET.loginbutton}
-              onPress={clickFunctionTestLoggedIn}
-            />
-            <Button 
-              title="Logout"
-              style={STYLE_SHEET.loginbutton} 
-              onPress={clickFunctionLogout}
-            />
-        </View>
+  };
+
+  return (
+    <View style={STYLE_SHEET.container}>
+      <Text style={STYLE_SHEET.header}>Hello User</Text>
+      <Text>Status: {api}</Text>
+      <View style={STYLE_SHEET.loginbuttonbox}>
+        <Button
+          title="TestLoggedIn"
+          style={STYLE_SHEET.loginbutton}
+          onPress={clickFunctionTestLoggedIn}
+        />
+        <Button
+          title="Logout"
+          style={STYLE_SHEET.loginbutton}
+          onPress={clickFunctionLogout}
+        />
       </View>
-    );
-  }
+    </View>
+  );
+}
