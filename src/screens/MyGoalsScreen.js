3cc7f2f0b233176e41f9dbe720682f63239bf139
Jordan Foss <j.foss@uqconnect.edu.au>
2020-10-07 14:29:40 +1000

    Made the searchbar and buttons on the budget page the actual things.

--- src/screens/MyGoalsScreen.js
+++ src/screens/MyGoalsScreen.js
@@ -1,11 +1,11 @@
-import React from "react";
-import { ScrollView } from "react-native";
+import React, { useState } from "react";
+import { ScrollView, TouchableOpacity } from "react-native";
 import { STYLESHEET } from "../styles/stylesheet";
 import { Text, View, Dimensions } from "react-native";
 import Colors from "../styles/colors";
 import BottomBar from "../components/BottomBar";
 import Pill from "../components/Pill";
-
+import { SearchBar } from 'react-native-elements';
 const style = {
     blueBubbleView: {
         backgroundColor: Colors.Primary,
@@ -32,6 +32,32 @@ const style = {
     
         ...STYLESHEET.shadowNormal,
       },
+      expensesButton: {
+        alignItems: 'center',
+        backgroundColor: Colors.Black,
+        padding: 10,
+        borderRadius: 10,
+        marginLeft: 5
+      },
+      incomeButton: {
+        alignItems: 'center',
+        backgroundColor: Colors.White,
+        padding: 10,
+        borderRadius: 10,
+        marginLeft: 5
+      },
+      searchBarInputStyle: {
+          backgroundColor: Colors.White,
+          fontSize: 16
+      },
+      searchBarContainerStyle: {
+        backgroundColor: Colors.White,
+        borderBottomColor: Colors.White,
+        borderTopColor: Colors.White,
+        height: 30,
+        paddingVertical: 0,
+        width: 130
+      },
       incomeView: {
         width: Dimensions.get("window").width - 40,
         flexDirection: 'row',
@@ -40,9 +66,6 @@ const style = {
       },
       incomeNameView: {
         width: Dimensions.get("window").width - 140,
-      },
-      amountView: {
-        width: 60,
       },
       sectioningView: {
         width: Dimensions.get("window").width - 40,
@@ -56,7 +79,8 @@ const style = {
       },
       thirdsSectioningViewWhite: {
         backgroundColor: Colors.White,
-        width: (Dimensions.get("window").width - 80)/3,
+        width: (Dimensions.get("window").width - 100)/3,
+        paddingVertical: 0,
       },
       textView: {
         backgroundColor: Colors.Primary,
@@ -91,6 +115,10 @@ const style = {
         fontSize: 14,
         color: Colors.White,
       },
+      defaultHeaderSmallBlack: {
+        fontSize: 14,
+        color: Colors.Black,
+      },
       defaulthLine: {
         borderBottomColor: Colors.LightGray,
         borderBottomWidth: 1,
@@ -105,7 +133,21 @@ const style = {
       }
 }
 
+function setButtonValue(value, set) {
+  if (value) {
+    set(false)
+  } else {
+    set(true)
+  }
+}
+
 export default function MyGoals( {navigation} ) {
+
+  const [state, setState] = useState("")
+  const [expenseButtonPressed, setExpenButton] = useState(false)
+  const [incomeButtonPressed, setIncomeButton] = useState(false)
+
+  const { search } = state;
 
   return <View style={STYLESHEET.defaultView}>
     <ScrollView>
@@ -161,19 +203,29 @@ export default function MyGoals( {navigation} ) {
     <View style={style.whiteBubbleView}>
       <View style={style.sectioningView}>
         <View style={style.thirdsSectioningViewWhite}>
-          <Text>
-            EXPENSES
-          </Text>
+          <TouchableOpacity 
+          style={style.expensesButton} 
+          onPress={(expenseButtonPressed) => setButtonValue(expenseButtonPressed, setExpenButton)}>
+            <Text style={style.defaultHeaderSmallWhite}>EXPENSES</Text>
+          </TouchableOpacity>
         </View>
         <View style={style.thirdsSectioningViewWhite}>
-          <Text>
-            INCOME
-          </Text>
+          <TouchableOpacity 
+          style={style.incomeButton}
+          onPress={(incomeButtonPressed) => setButtonValue(incomeButtonPressed, setIncomeButton)}>
+            <Text style={style.defaultHeaderSmallBlack}>INCOME</Text>
+          </TouchableOpacity>
         </View>
         <View style={style.thirdsSectioningViewWhite}>
-          <Text>
-            SEARCH BAR
-          </Text>
+        <SearchBar
+          round={true}
+          containerStyle={style.searchBarContainerStyle}
+          inputContainerStyle={style.searchBarContainerStyle}
+          inputStyle={style.searchBarInputStyle}
+          placeholder="Search..."
+          onChangeText={(state) => setState({search})}
+          value={state}
+        />
         </View>
       </View>
       <Text style={style.defaulthLineBlack}/>
@@ -187,7 +239,7 @@ export default function MyGoals( {navigation} ) {
             Grocery Shopping
           </Text>
         </View>
-        <View style={style.amountView}>
+        <View width={60}>
             <Text>
               Money
             </Text>
