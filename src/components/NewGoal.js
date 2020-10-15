6b28aefcc8bba0e0694add61e753cf7beebb9dfd
Jordan Foss <j.foss@uqconnect.edu.au>
2020-10-15 11:03:38 +1000

    New goal thing looks good, but the create goal button isn't working yet.

--- src/components/NewGoal.js
+++ src/components/NewGoal.js
@@ -1,5 +1,5 @@
 import React, { useContext, useEffect, useState } from 'react';
-import { Image, Text, View, TouchableOpacity } from 'react-native';
+import { Image, Text, View, TouchableOpacity, Picker} from 'react-native';
 import AppContext from '../helper/context';
 import Paycheck from './Paycheck';
 import { Dimensions } from "react-native";
@@ -11,6 +11,7 @@ import {
 } from "../styles/typography";
 import { faExclamationTriangle } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
+import paycheckStyle from './PaychecksReceived.style';
 
 // Icon assets
 import CloudOne from '../assets/cloud.svg';
@@ -22,121 +23,85 @@ import PlanetTwo from '../assets/planet2.svg';
 import PlanetRing from '../assets/planetRing.svg';
 import { TextInput } from 'react-native-gesture-handler';
 
-const paycheckStyle = {
-    card: {
-      width: Dimensions.get("window").width - 50,
-      borderRadius: 20,
-      backgroundColor: "white",
-      padding: 0,
-      position: "relative",
-    },
-    header: {
-      width: Dimensions.get("window").width - 50,
-      height: 120,
-      backgroundColor: Colors.Black,
-      padding: 20,
-      paddingTop: 35,
-      flex: 0,
-      alignItems: "center",
-      borderTopLeftRadius: 20,
-      borderTopRightRadius: 20,
-    },
-    headerText: {
-      fontFamily: FONT_FAMILY_SEMIBOLD,
-      fontSize: 18,
-      color: "white",
-      textAlign: "center",
-    },
-    planetOne: {
-      position: "absolute",
-      left: 10,
-      top: -40,
-    },
-    planetTwo: {
-      position: "absolute",
-      right: -20,
-      top: 40,
-    },
-    planetRing: {
-      position: "absolute",
-      top: -30,
-      right: 30,
-    },
-    info: {
-      backgroundColor: Colors.White,
-      position: "absolute",
-      top: 75,
-      left: (Dimensions.get("window").width - 50) / 2,
-      width: 320,
-      borderRadius: 15,
-      marginLeft: -160,
-      //   transform: [{ translateX: (Dimensions.get('window').width - 50) / 2 }],
-      ...STYLESHEET.shadowNormal,
-    },
-    infoText: {
-      color: Colors.Black,
-      fontFamily: FONT_FAMILY_REGULAR,
-      fontSize: 13,
-      padding: 20,
-    },
-    content: {
-      marginTop: 50,
-      padding: 30,
-      flex: 0,
-      flexDirection: "column",
-    },
-    title: {
-      fontFamily: FONT_FAMILY_SEMIBOLD,
-      color: "#565758",
-    },
-    paycheckWrapper: {
-      flex: 0,
-      flexDirection: "column",
-      marginBottom: 30,
-    },
-    buttonWrapper: {
-      flex: 0,
-      paddingTop: 50,
-      flexDirection: "row",
-    },
-    touchable: {
-      borderRadius: 15,
-      marginRight: 10,
-      ...STYLESHEET.shadowNormal,
-    },
-    button: {
-      borderRadius: 15,
-      backgroundColor: Colors.PrimaryLighter,
-      paddingTop: 20,
-      paddingBottom: 20,
-      flex: 1,
-      width: 150,
-      flexDirection: "row",
-      alignItems: "center",
-      justifyContent: "center",
-    },
-    buttonText: {
-      fontFamily: FONT_FAMILY_SEMIBOLD,
-      fontSize: 12,
-      color: "white",
-    },
-    cancelButton: {
-      backgroundColor: Colors.Alert,
-    },
-    completeButton: {
-      backgroundColor: Colors.Black,
-    },
-  };
-
+const newGoalStyle = {
+  header: {
+    width: Dimensions.get("window").width - 50,
+    height: 120,
+    backgroundColor: Colors.Black,
+    padding: 20,
+    paddingTop: 35,
+    flex: 0,
+    alignItems: "center",
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
+  },
+  planetOne: {
+    position: "absolute",
+    left: 10,
+    top: -40,
+  },
+  planetTwo: {
+    position: "absolute",
+    right: -20,
+    top: 40,
+  },
+  planetRing: {
+    position: "absolute",
+    top: -30,
+    right: 30,
+  },
+  info: {
+    backgroundColor: Colors.White,
+    position: "absolute",
+    top: 75,
+    left: (Dimensions.get("window").width - 50) / 2,
+    width: 320,
+    borderRadius: 15,
+    marginLeft: -160,
+    //   transform: [{ translateX: (Dimensions.get('window').width - 50) / 2 }],
+    ...STYLESHEET.shadowNormal,
+  },
+  infoText: {
+    color: Colors.Black,
+    fontFamily: FONT_FAMILY_REGULAR,
+    fontSize: 13,
+    padding: 20,
+  },
+  content: {
+    marginTop: 50,
+    padding: 30,
+    flex: 0,
+    flexDirection: "column",
+  },
+  buttonWrapper: {
+    flex: 0,
+    paddingTop: 50,
+    flexDirection: "row",
+  },
+  optionsText: {
+    fontFamily: FONT_FAMILY_SEMIBOLD,
+    color: Colors.Black,
+  },
+  cancelButton: {
+    backgroundColor: Colors.Alert,
+  },
+  completeButton: {
+    backgroundColor: Colors.Black,
+  }
+}
   const iconStyle = {
     opacity: 0.8,
   };
 
-const PaychecksReceived = ({ onClose, onComplete }) => {
+const NewGoal = ({ onClose, goal }) => {
   const Context = useContext(AppContext);
 
-  const [goalName, setGoalName] = useState("");
+  const goalTypes = ["One-Off", "Continuous"];
+  const [selected, setSelected] = useState("One-Off");
+
+  const [goalName, setGoalName] = useState(goal);
   const [goalAmount, setGoalAmount] = useState("");
+  const [goalType, setGoalType] = useState("");
   const [fornightlyGoal, setFortnightlyGoal] = useState("");
   const [completionDate, setCompletionDate] = useState("");
 
@@ -155,63 +120,69 @@ const PaychecksReceived = ({ onClose, onComplete }) => {
 
   return (
     <View style={paycheckStyle.card}>
-      <View style={paycheckStyle.header}>
+      <View style={newGoalStyle.header}>
         <Text style={paycheckStyle.headerText}>Start New Saving Goal</Text>
-        <PlanetOne style={paycheckStyle.planetOne} width={80} height={80} />
-        <PlanetTwo style={paycheckStyle.planetTwo} width={80} height={80} />
-        <PlanetRing style={paycheckStyle.planetRing} width={60} height={60} />
+        <PlanetOne style={newGoalStyle.planetOne} width={80} height={80} />
+        <PlanetTwo style={newGoalStyle.planetTwo} width={80} height={80} />
+        <PlanetRing style={newGoalStyle.planetRing} width={60} height={60} />
       </View>
-      <View style={paycheckStyle.info}>
-        <Text style={paycheckStyle.infoText}>
+      <View style={newGoalStyle.info}>
+        <Text style={newGoalStyle.infoText}>
           Create a goal to start saving towards a new target. 
           Deigo will allocate money into your goal every fortnight, and track your progress each step of the way
         </Text>
       </View>
-      <View style={paycheckStyle.content}>
+      <View style={newGoalStyle.content}>
         <View paddingBottom={30}>
-            <Text>
-                Goal Name
+            <Text style={newGoalStyle.optionsText}>
+                Goal name
             </Text>
             <TextInput
-                placeholder="Enter goal name"
+              style={{color: Colors.Black, fontSize: 16, height: 40, borderBottomWidth: 0.5, borderBottomColor: Colors.MediumGray}}
+              placeholder={goalName}
             />
         </View>
         <View flexDirection={"row"}>
             <View width={(Dimensions.get("window").width - 50)/2}>
-                <Text>
-                    Target Amount
+                <Text style={newGoalStyle.optionsText}>
+                    Target amount
                 </Text>
             </View>
-            <Text>
+            <Text style={newGoalStyle.optionsText}>
                 Type
             </Text>
         </View>
         <View flexDirection={"row"} paddingBottom={20}>
-            <View width={(Dimensions.get("window").width - 50)/2} flexDirection={"row"}>
-                <Text>$</Text>
+            <View style={{width: (Dimensions.get("window").width - 70)/2, flexDirection: "row"}}>
+                <Text style={{fontFamily: FONT_FAMILY_SEMIBOLD, color: Colors.Black, fontSize: 20, paddingRight: 20}}>$</Text>
                 <TextInput
-                    placeholder="Enter goal name"
+                  placeholder="2500.0"
                 />
             </View>
-            <TextInput
-                placeholder="Enter goal name"
-            />
+            <Picker
+              selectedValue={selected}
+              style={{ height: 20, width: 150 }}
+              onValueChange={(itemValue) => setSelected(itemValue)}
+            >
+              <Picker.Item label="One-Off" value="one-off" />
+              <Picker.Item label="Continuous" value="continuous" />
+            </Picker>
         </View>
         <View flexDirection={"row"}>
             <View width={(Dimensions.get("window").width - 50)/2}>
-                <Text>
+                <Text style={newGoalStyle.optionsText}>
                     Allocate per fortnight
                 </Text>
             </View>
-            <Text>
+            <Text style={newGoalStyle.optionsText}>
                 Complete by
             </Text>
         </View>
         <View flexDirection={"row"} paddingBottom={20}>
             <View width={(Dimensions.get("window").width - 50)/2} flexDirection={"row"}>
-                <Text>$</Text>
+                <Text style={{fontFamily: FONT_FAMILY_SEMIBOLD, color: Colors.Black, fontSize: 20, paddingRight: 20}}>$</Text>
                 <TextInput
-                    placeholder="Enter goal name"
+                    placeholder="250.00"
                 />
             </View>
             <TextInput
@@ -224,14 +195,15 @@ const PaychecksReceived = ({ onClose, onComplete }) => {
                 icon={faExclamationTriangle}
                 size={Dimensions.get("window").height * 0.02}
                 color={Colors.DarkGray}
+                marginRight={10}
             />
-            <Text>
+            <Text style={{fontSize: 12, color: Colors.DarkGray}}>
                 You'll likely need more time to complete this goal
             </Text>
         </View>
-        <View style={paycheckStyle.buttonWrapper}>
+        <View style={newGoalStyle.buttonWrapper}>
           <TouchableOpacity style={paycheckStyle.touchable} onPress={onClose}>
-            <View style={[paycheckStyle.button, paycheckStyle.cancelButton]}>
+            <View style={[paycheckStyle.button, newGoalStyle.cancelButton]}>
               <Text style={paycheckStyle.buttonText}>Cancel</Text>
             </View>
           </TouchableOpacity>
@@ -240,7 +212,7 @@ const PaychecksReceived = ({ onClose, onComplete }) => {
             style={paycheckStyle.touchable}
             onPress={createGoal}
           >
-            <View style={[paycheckStyle.button, paycheckStyle.completeButton]}>
+            <View style={[paycheckStyle.button, newGoalStyle.completeButton]}>
               <Text style={paycheckStyle.buttonText}>Create Goal</Text>
             </View>
           </TouchableOpacity>
@@ -250,4 +222,4 @@ const PaychecksReceived = ({ onClose, onComplete }) => {
   );
 };
 
-export default PaychecksReceived;
+export default NewGoal;
